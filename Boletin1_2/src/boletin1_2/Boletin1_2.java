4b5bd29ac83a0af81967be4632a3d69f002d104c
aguinaldogonzalez <aguinaldogonzalez@nautilus4.danielcastelao.org>
2018-12-17 11:59:41 +0100

    c.

---
+++ Boletin1_2/src/boletin1_2/Boletin1_2.java
@@ -0,0 +1,17 @@
+
+package boletin1_2;
+
+
+public class Boletin1_2 {
+
+    
+    public static void main(String[] args) {
+        
+        int lado=3;
+        System.out.println("Area do cuadrado = "+lado*lado);
+            
+              
+        
+    }
+    
+}
